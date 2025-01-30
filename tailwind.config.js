e158736222fa6805ddc88da9fb3cf81eeebd2e00
IqbalM42 <mhiqbal1982@gmail.com>
2025-01-30 23:24:40 +0600

    my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// font family
+// font-family: "ADLaM Display", serif;
+// font-family: "Sura", serif; 
